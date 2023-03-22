0388673d1d7f11942959435ddddc853f29bbb0b0
Fernando Ayats <ayatsfer@gmail.com>
2023-03-22 23:23:47 +0000

    query openai

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ use async_trait::async_trait;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 use std::sync::Mutex;
-use tracing::info;
-use tracing::warn;
+use tracing::{debug, info};
 use tracing::{span, Level};
+use tracing::{trace, warn};
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::{layer::SubscriberExt, Registry};
 use tracing_tree::HierarchicalLayer;
@@ -18,46 +18,111 @@ use tracing_tree::HierarchicalLayer;
 #[derive(Debug)]
 struct Handler {
     msg_queue: Arc<Mutex<HashMap<ChannelId, VecDeque<Message>>>>,
+    ai_handler: async_openai::Client,
 }
 
 impl Handler {
     fn new() -> Self {
         Self {
             msg_queue: Arc::new(Mutex::new(HashMap::new())),
+            ai_handler: async_openai::Client::new(),
         }
     }
 }
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         info!("{} ready", ready.user.name);
     }
 
-    async fn message(&self, _: Context, msg: Message) {
+    async fn message(&self, ctx: Context, msg: Message) {
         let _span = span!(Level::INFO, "msg");
         info!(?msg);
 
-        match self.msg_queue.clone().lock() {
+
+        let ai_input = match self.msg_queue.clone().lock() {
             Err(err) => {
                 warn!(?err, "Mutex poisoned");
+                None
             }
-            Ok(mut guard) => {
-                match guard.entry(msg.channel_id) {
+            Ok(mut msg_queue) => {
+                match msg_queue.entry(msg.channel_id) {
                     Entry::Vacant(entry) => {
-                        entry.insert(VecDeque::from([msg]));
+                        entry.insert(VecDeque::from([msg.clone()]));
+                        None
                     }
                     Entry::Occupied(mut entry) => {
-                        let entry = entry.get_mut();
-                        entry.push_back(msg);
-                        if entry.len() >= 5 {
-                            entry.pop_front();
+                        let vec = entry.get_mut();
+                        vec.push_back(msg.clone());
+                        if vec.len() >= 10 {
+                            vec.pop_front();
+                        }
+                        // let vec = *&vec;
+                        build_msg(vec.clone())
+                    }
+                }
+            }
+        };
+
+        if msg.author.bot {
+            return;
+        }
+
+        if msg.content.contains("bot") {
+            if let Some(ai_input) = ai_input {
+                match self.ai_handler.chat().create(ai_input).await {
+                    Err(err) => warn!(?err, "Didn't get openai response"),
+                    Ok(responses) => {
+                        info!(?responses, "Ok response");
+                        for r in responses.choices {
+                            match r.message.role {
+                                async_openai::types::Role::Assistant => {
+                                    msg.channel_id
+                                        .say(&ctx.http, r.message.content)
+                                        .await
+                                        .unwrap();
+                                }
+                                _ => warn!("Didn't receive an assistant response"),
+                            }
                         }
                     }
                 };
-                info!(?guard);
             }
         };
+    }
+}
+
+#[tracing::instrument]
+fn build_msg<I: IntoIterator<Item = Message> + std::fmt::Debug>(
+    stack: I,
+) -> Option<async_openai::types::CreateChatCompletionRequest> {
+    use async_openai::types::{ChatCompletionRequestMessage, ChatCompletionRequestMessageArgs};
+
+    let msgs: Result<Vec<ChatCompletionRequestMessage>, _> = stack
+        .into_iter()
+        .map(|m| {
+            ChatCompletionRequestMessageArgs::default()
+                .role(async_openai::types::Role::User)
+                .content(m.content)
+                .build()
+        })
+        .collect();
+
+    let context = vec![ChatCompletionRequestMessageArgs::default()
+        .role(async_openai::types::Role::System)
+        .content("Eres un bot muy útil en una sala de chat online. Estás en una conversación entre varios usuarios y tienes que responderlos")
+        .build()
+        .unwrap()];
+
+    match msgs {
+        Err(_) => return None,
+        Ok(msgs) => async_openai::types::CreateChatCompletionRequestArgs::default()
+            .model("gpt-3.5-turbo")
+            .messages(context)
+            .messages(msgs)
+            .build()
+            .ok(),
     }
 }
 
