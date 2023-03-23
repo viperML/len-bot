d7fd8c2852d787745a097d41fdcb0e617acccb9a
Fernando Ayats <ayatsfer@gmail.com>
2023-03-23 09:06:33 +0100

    add jq

--- nomad.sh
+++ nomad.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S nix shell nixpkgs#nomad --command bash
+#!/usr/bin/env -S nix shell nixpkgs#nomad nixpkgs#jq --command bash
 
 export NOMAD_VAR_rev=$(nix flake metadata . --json | jq -r '.locked.rev')
 export NOMAD_VAR_narHash=$(nix flake metadata . --json | jq -r '.locked.narHash')
